e288a8ca70ae2b5b59ce960597b5c1471316361a
kylekewley <kylek61294@gmail.com>
2014-09-19 17:56:48 -0700

    Overloading the == operator

--- include/Differentiable.h
+++ include/Differentiable.h
@@ -9,6 +9,10 @@ namespace calculator {
         public:
         virtual ~Differentiable() {}
 
+        virtual bool operator==(const Differentiable&) const {
+            std::cerr << "Error: Subclasses should override the == operator" << std::endl;
+            return false;
+        }
         virtual Differentiable derivative() const {
             std::cerr << "Error: Subclasses should override the derivative method in Differentiable" << std::endl;
             return Differentiable();
